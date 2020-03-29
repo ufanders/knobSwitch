eabe747512a48e77bdada3cd9a88640081bfcff1
Anders Nelson <anders.k.nelson@gmail.com>
2020-03-29 17:55:22 -0400

    Builds and runs well. -Removed SR8500 stuff. -Notifications work. -Migrated to M5Stack board.

--- Arduino/knobSwitch_ble_server/knobSwitch_ble_server.ino
+++ Arduino/knobSwitch_ble_server/knobSwitch_ble_server.ino
@@ -7,6 +7,8 @@
 #include "esp_bt_device.h"
 #include <Arduino.h>
 #include "esp32-hal-log.h"
+#include <BLE2902.h>
+#include <M5Stack.h>
 
 int keypadPin = 36;
 char keyOld;
@@ -17,60 +19,8 @@ unsigned long time_now = 0;
 
 #define SERVICE_UUID "18550d7d-d1aa-4968-a563-e8ebeb4840ea"
 
-struct sr8500_property_map {
-  char uuid[37];
-  char statusStr[4];
-};
-
 //NOTE: The STANDBY MODE must be set to NORMAL 
 //instead of ECONOMY for the power function to work correctly.
-
-//we want to keep this info in flash, not in RAM.
-const sr8500_property_map sr8500Map[] = {
-  {"7b0819db-21f6-429d-ad5a-c31215fc7114", "PWR"},
-  {"1ffacfe7-6051-42b1-8dab-004e164a82cd", "VOL"},
-  {"d1cf0f01-a78d-4de0-9812-bd391bcba635", "AMT"},
-  {"18a36e61-32fb-4f9d-932f-75c246f1f881", "TOB"},
-  {"01f55e00-5b3b-443f-8ca2-24f2b3ca72fb", "TOT"},
-  {"d33efca9-e30a-4e41-98fc-d5c84e2b3f77", "SRC"},
-  {"d878b661-6cae-48e8-a84c-3952f89f5623", "71C"}, 
-  {"25f76a7b-87e3-498e-9abf-290fb89c16c7", "INP"},
-  {"c7d89e80-42b2-4b0e-8b6b-a783258e48e5", "ATT"}, 
-  {"b7dae477-dd0d-4f0e-afdb-229e3bdb983a", "SUR"}, 
-  {"6ac062d9-b0a2-4483-800e-a6497c7365ab", "THX"}, 
-  {"f4544ed7-17fa-4d7d-a826-8513132eb31b", "SPK"}, 
-  {"15095cae-1aca-423b-ba62-609f8bf6c0ac", "TFQ"}, 
-  {"a89367a0-9e05-461d-9a07-10b8135f3a46", "TPR"}, 
-  {"7e8f0681-a312-4934-9300-65de924e4ff0", "TMD"}, 
-  {"ef8ce36d-1e04-4d4a-845d-d71707201257", "MNU"},
-  {"8981aff4-52c3-4e3b-9be6-dc11af1d4971", "CUR"},
-  {"5e30b0bc-d3a3-45a6-a95b-ad5bfb6647a5", "TTO"}, 
-  {"1bf5ef56-90d4-46d5-87b8-35c6d071a01b", "CHL"}, 
-  {"eeea6f37-db29-44d0-bd4d-7c45641fa92f", "NGT"}, 
-  {"f46b3732-cd2c-4a3d-8301-1b7167d7fd54", "REQ"}, 
-  {"6535a6f2-961a-4f7c-af67-6192be58bd8e", "SLP"}, 
-  {"b276067f-6e05-44ef-8231-2853a8f6a405", "SIG"}, 
-  {"763cf4bc-e298-4fe1-b70a-f2029ff21f8d", "SFQ"}, 
-  {"fb7478cf-7f59-4056-9796-ae9768956b4e", "CHS"},
-  {"7fb93eb2-ed8f-4051-9497-15feb0e0db84", "MPW"}, 
-  {"58110e30-5916-4c04-a71e-729ccd58147b", "MVL"}, 
-  {"ee4b9c81-d628-415d-9fd2-b53e3bb2280b", "MAM"}, 
-  {"93521329-e117-41c7-97c7-e3cd466457e7", "MVS"}, 
-  {"1ecd0948-e5f9-402c-b342-611417ccf8cc", "MSP"}, 
-  {"dd3361c4-a86a-42f3-8d77-8caaad5ded17", "MSV"}, 
-  {"f1b68440-406c-4c0b-8b52-921947143d2b", "MSM"}, 
-  {"d72a2014-1cc1-4fbc-ba51-ec07dc4c4af4", "MST"},  
-  {"c67b1a61-1ac9-43b0-a5c1-8228ab12effd", "MSC"}, 
-  {"34eba641-6f2e-446a-8755-ea1d0fd30d62", "MTF"}, 
-  {"7bb61d7e-5cac-4b90-8982-1972975b12b9", "MTP"}, 
-  {"5731a6c9-64c6-41ee-9fd3-0cc3d0bd7692", "MTM"},
-  {"85cd9a55-5766-4a26-aac4-be7054b0c4b0", "OSD"},
-  {"44e017cb-f131-4c56-a75e-3f1b499319ac", "AST"}
-};
-
-#define SR8500_NUMCHARS (sizeof(sr8500Map)/sizeof(sr8500_property_map))
-
-BLECharacteristic* sr8500_chars_ptr[SR8500_NUMCHARS];
 
 struct sr5010_property_map {
   char uuid[37];
@@ -128,9 +78,6 @@ char sr5010_searchByUUID(const char*);
 char sr5010_searchByCmd(char*);
 char sr5010_searchByArg(int cmdIndex, char* ptrArg);
 
-char sr8500_searchByUUID(const char*);
-char sr8500_searchByCmd(char*);
-
 char rxBuf[32], rxBufIdx, rxBufLen;
 
 bool deviceConnected = false;
@@ -177,7 +124,7 @@ class MyCallbacks: public BLECharacteristicCallbacks {
       
         //write to serial port.
         Serial.printf("-> %s%s\n", sr5010Map[i].statusStr, sr5010MapArgsOut[argIndex]);
-        Serial1.printf("%s%s\r", sr5010Map[i].statusStr, sr5010MapArgsOut[argIndex]);
+        Serial2.printf("%s%s\r", sr5010Map[i].statusStr, sr5010MapArgsOut[argIndex]);
       }
     };
 };
@@ -208,11 +155,14 @@ BLEService *pService;
 void setup() {
   Serial.setDebugOutput(true);
   esp_log_level_set("*",ESP_LOG_VERBOSE);
+
+  //disable M5Stack speaker clicking noise.
+  dacWrite(25,0);
   
   Serial.begin(115200);
   Serial.println("Starting BLE.");
 
-  Serial1.begin(9600, SERIAL_8N1, 5, 4); //pin 4=TXD, pin 5=RXD.
+  Serial2.begin(9600, SERIAL_8N1, 16, 17); //pin 16=TXD, pin 17=RXD.
 
   //INFO: https://github.com/nkolban/esp32-snippets/issues/144
   BLEDevice::init("SR5010");
@@ -232,19 +182,14 @@ void setup() {
     sr5010_chars_ptr[i] = pService->createCharacteristic(sr5010Map[i].uuid,
     BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE \
     | BLECharacteristic::PROPERTY_NOTIFY); // | BLECharacteristic::PROPERTY_INDICATE);
+
+    // Create a BLE Descriptor so notifications work.
+    sr5010_chars_ptr[i]->addDescriptor(new BLE2902());
+  
     sr5010_chars_ptr[i]->setCallbacks(new MyCallbacks());
   }
   Serial.printf("Created %u characteristics.\n", i);
 
-/*
-  for(int i = SR5010_NUMCHARS; i<SR5010_NUMCHARS; i++)
-  {
-    sr5010_chars_ptr[i] = pService->createCharacteristic(sr5010Map[i].uuid,
-    BLECharacteristic::PROPERTY_READ);
-    sr5010_chars_ptr[i]->setCallbacks(new MyCallbacks());
-  }
-*/
-  
   pService->start();
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
   pAdvertising->addServiceUUID(pService->getUUID());
@@ -253,12 +198,12 @@ void setup() {
   pAdvertising->setMinPreferred(0x12);
   pAdvertising->start();
   Serial.println("Started BLE.");
-  
+
   //read/refresh all statuses.
-  for(i=0; i<SR5010_NUMCHARS-2; i++)
+  for(i=0; i<SR5010_NUMCHARS; i++)
   {
     Serial.printf("-> %s?\n", sr5010Map[i].statusStr);
-    Serial1.printf("%s?\r", sr5010Map[i].statusStr);
+    Serial2.printf("%s?\r", sr5010Map[i].statusStr);
     while(!receiveUpdateSerial());
     processUpdateSerial(rxBuf);
   }
@@ -275,8 +220,12 @@ void loop() {
   char strTemp[16] = "";
   bool keySuppress = false;
 
+  //Serial.println("Loop");
+
   if(millis() >= time_now + 100) //every 100ms
   {
+    //Serial.println("KeyScan");
+    
     keyNew = analogKeypadUpdate(keypadPin);
     if(keyOld != keyNew)
     {
@@ -334,14 +283,16 @@ void loop() {
       if(keyNew <= 5)
       {
         Serial.printf("-> %.5s\n", strTemp);
-        Serial1.printf("%.5s\r", strTemp);
+        Serial2.printf("%.5s\r", strTemp);
       }
       
       if(keyNew >= 6)
       {
         Serial.printf("-> %.9s\n", strTemp);
-        Serial1.printf("%.9s\r", strTemp);
+        Serial2.printf("%.9s\r", strTemp);
       }
+
+      keySuppress = false;
     }
     
     keyPressed = false;
@@ -438,14 +389,14 @@ int receiveUpdateSerial(void)
   char rc;
 
   //receive any new status update.
-  if(Serial1.available())
+  if(Serial2.available())
   {
     rxBufIdx = 0; rxBufLen = 0;
     
     //read in characters until we see a CR.
-    while(Serial1.available())
+    while(Serial2.available())
     {
-      rc = Serial1.read();
+      rc = Serial2.read();
       if(rc != '\r') rxBuf[rxBufIdx++] = rc; 
       else 
       {
@@ -456,52 +407,6 @@ int receiveUpdateSerial(void)
   }
   
   return 0;
-}
-
-char sr8500_searchByCmd(char* ptrCmd)
-{
-  char retVal = 0xFF; //No match found.
-
-  //search for the characteristic the received status update corresponds to.
-  int i = 0;
-  int c = 0;
-  int match = 0;
-  while(i<SR8500_NUMCHARS && !match)
-  {
-    c = memcmp(ptrCmd, sr8500Map[i].statusStr, 3);
-    
-    if(c == 0) //we found a match.
-    {
-      match = 1;
-      retVal = i;
-    }
-    else i++;
-  }
-
-  return retVal;
-}
-
-char sr8500_searchByUUID(const char* ptrUUID)
-{
-  char retVal = 0xFF; //No match found.
-
-  //search for the command the written characteristic maps to.
-  int i = 0;
-  int c = 0;
-  int match = 0;
-  while(i<SR8500_NUMCHARS && !match)
-  {
-    c = strcmp(ptrUUID, sr8500Map[i].uuid);
-    
-    if(c == 0) //we found a match.
-    {
-      match = 1;
-      retVal = i;
-    }
-    else i++;
-  }
-
-  return retVal;
 }
 
 char sr5010_searchByCmd(char* ptrCmd)
@@ -601,7 +506,7 @@ char sr5010_searchByUUID(const char* ptrUUID)
 }
 
 int keypadLut[] = {
-  1023, 970, 850, 765,
+  1023, 1010, 850, 765,
   642, 605, 567, 538,
   474, 442, 429, 398,
   362, 287, 231, 199, 0
@@ -620,6 +525,8 @@ char analogKeypadUpdate(int analogPin)
     analogValue += analogRead(keypadPin) >> 2; //reduce to 10-bit value
   }
   analogValue /= 8; //get oversampled average.
+
+  //Serial.printf("anaVal:%d\n", analogValue);
   
   for(i=0; i<(sizeof(keypadLut)/sizeof(keypadLut[0])); i++)
   {
