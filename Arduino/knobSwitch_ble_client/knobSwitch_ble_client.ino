e4e660b56356bc3bc68d7ccf517947478672d775
Anders Nelson <anders.k.nelson@gmail.com>
2020-04-24 23:25:11 -0400

    Builds and runs well. -Cleaned up/added button handlers.

--- Arduino/knobSwitch_ble_client/knobSwitch_ble_client.ino
+++ Arduino/knobSwitch_ble_client/knobSwitch_ble_client.ino
@@ -3,11 +3,13 @@
 #include <SimpleRotary.h>
 #include <M5Stack.h>
 #include <string.h>
+#include <AnalogKeypad.h>
 
 unsigned long time_now = 0;
 char batteryLevel, retries;
 volatile char updateBitfield;
 bool sleepTimerExpired, updateLcd;
+volatile bool updateAll;
 
 #define bitPwr (1 << 0)
 #define bitVolume (1 << 1)
@@ -16,39 +18,52 @@ bool sleepTimerExpired, updateLcd;
 #define bitSrcVideo (1 << 4)
 
 // Pin A, Pin B, Button Pin
-SimpleRotary rotary(35,36,26);
+SimpleRotary rotary(2,5,17);
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
 
 #define SERVICE_UUID "18550d7d-d1aa-4968-a563-e8ebeb4840ea"
 
 //NOTE: The STANDBY MODE must be set to NORMAL 
 //instead of ECONOMY for the power function to work correctly.
 
-//we want to keep this info in flash, not in RAM.
+#define BIT_ISUPDOWN 0b1
+
 struct sr5010_property_map {
   char uuid[37];
   char statusStr[9]; //up to 8-character prefix (wtf!)
   int numArgs;
+  char attributes; //bitfield
 };
 
+//we want to keep this info in flash, not in RAM.
 const sr5010_property_map sr5010Map[] = {
-  {"bb0dae34-05cb-4290-a8c4-6dad813e82e2", "PW", 2},
-  {"88e1ef43-9804-411d-a1f9-f6cdbda9a9a9", "MV", 2},
-  {"60ef9a3d-5a67-464a-b7bf-fe1c187828f1", "MU", 2},
-  {"c21d2540-fe05-43d7-b929-54b76140e030", "SI", 4},
-  {"a6b89a0b-c740-4b82-bba5-df717b4163d5", "SV", 4},
-  {"0969d2b6-60d8-4a1b-a88c-8457449a453e", "TFHD", 0},
-  {"0969d2b6-60d8-4a1b-a88c-8457449a453e", "TFAN", 0},
-  {"153acaae-9181-48c5-908c-21e81d3a8f85", "Z2", 2},
+  {"bb0dae34-05cb-4290-a8c4-6dad813e82e2", "PW", 2, 0},
+  {"3ac1dc48-4824-4e03-8f5e-9b4a591a94e9", "ZM", 2, 0},
+  {"88e1ef43-9804-411d-a1f9-f6cdbda9a9a9", "MV", 2, 0},
+  {"60ef9a3d-5a67-464a-b7bf-fe1c187828f1", "MU", 2, 0},
+  {"c21d2540-fe05-43d7-b929-54b76140e030", "SI", 13, 0},
+  {"a6b89a0b-c740-4b82-bba5-df717b4163d5", "SV", 10, 0},
+  {"0969d2b6-60d8-4a1b-a88c-8457449a453e", "TFHD", 0, 0},
+  {"e377cf47-0f23-4004-ba4d-1f93c0193fca", "TFAN", 0, 0},
+  {"153acaae-9181-48c5-908c-21e81d3a8f85", "Z2", 2, 0},
+  //==== Increment-only characteristics
+  {"edfc239e-ea86-472b-8d95-0bd9d613b56c", "MV", 0, BIT_ISUPDOWN}, //MV up/down
+  {"2478b23b-6464-45dd-a0c5-9eb4923fd4aa", "Z2", 0, BIT_ISUPDOWN}, //Z2 up/down
 };
 
 //{outgoing argument strings}
-const char sr5010MapArgsOut[][8] = {
+const char sr5010MapArgsOut[][10] = {
   "ON", "STANDBY",
+  "ON", "OFF", //ZM
   "UP", "DOWN",
   "ON", "OFF",
-  "CD", "TV", "TUNER", "HDRADIO",
-  "OFF", "TV", "BD", "V.AUX",
-  "ON", "OFF"
+  //NOTE: Net/online music/usb/ipod/bluetooth functions return large strings that are custom-terminated.
+    //This is stupid, so we skip them for now.
+  "CD", "DVD", "BD", "TV", "SAT/CBL", "MPLAY", "GAME", "TUNER", /*"IRADIO", "SERVER", "FAVORITES",*/ "AUX1", "AUX2", "INET", "BT", "USB/IPOD", 
+  "DVD", "BD", "TV", "SAT/CBL", "MPLAY", "GAME", "AUX1", "AUX2", "CD", "OFF",
+  "ON", "OFF" //Z2
 };
 
 #define SR5010_NUMCHARS (sizeof(sr5010Map)/sizeof(sr5010_property_map))
@@ -87,7 +102,33 @@ static void notifyCallback(
   sr5010MapLocalState[charIndex] = atoi((const char*)pData);
   
   Serial.printf("Char[%d]: %d\n", charIndex, sr5010MapLocalState[charIndex]);
-  updateBitfield |= (1 << charIndex);
+
+  switch(charIndex)
+  {
+    case 0:
+      updateBitfield |= bitPwr;
+    break;
+
+    case 2:
+      updateBitfield |= bitVolume;
+    break;
+
+    case 3:
+      updateBitfield |= bitMute;
+    break;
+
+    case 4:
+      updateBitfield |= bitSrcAudio;
+    break;
+
+    case 5:
+      updateBitfield |= bitSrcVideo;
+    break;
+    
+    default:
+    break;
+  }
+
   updateLcd = true;
 }
 
@@ -146,7 +187,10 @@ bool connectToServer() {
     //register all characteristics for realtime notification.
     for(i = 0; i < SR5010_NUMCHARS; i++)
     {
+      if(!(sr5010Map[i].attributes & BIT_ISUPDOWN))
+      {
         pRemoteCharacteristic = pRemoteService->getCharacteristic(sr5010Map[i].uuid);
+        
         if (pRemoteCharacteristic != nullptr) 
         {
           if(pRemoteCharacteristic->canNotify())
@@ -159,21 +203,32 @@ bool connectToServer() {
             Serial.printf("%s can NOT notify.\n", sr5010Map[i].uuid);
           }
         }
+      }
+      else Serial.printf("%s will NOT notify.\n", sr5010Map[i].uuid);
     }
 
     //read remote
     for(i = 0; i < SR5010_NUMCHARS; i++)
     {
+      Serial.printf("<~ %s: ", sr5010Map[i].uuid);
+      
       pRemoteCharacteristic = pRemoteService->getCharacteristic(sr5010Map[i].uuid);
       
-      if (pRemoteCharacteristic != nullptr) 
+      if(pRemoteCharacteristic != nullptr) 
       {
-        if(pRemoteCharacteristic->canRead()) {
-          std::string val = pRemoteCharacteristic->readValue();
-          sr5010MapLocalState[i] = atoi(val.c_str());
-          Serial.printf("<- %s\n ", val.c_str());
+        if(!(sr5010Map[i].attributes & BIT_ISUPDOWN))
+        {
+          if(pRemoteCharacteristic->canRead()) {
+              std::string val = pRemoteCharacteristic->readValue();
+              sr5010MapLocalState[i] = atoi(val.c_str());
+              Serial.printf("%d\n ", sr5010MapLocalState[i]);
+            }
         }
+        else Serial.println("is UP/DOWN, skipping.");
+        
       }
+      else Serial.println("not found.");
+      
     }
  
     connected = true;
@@ -211,9 +266,18 @@ char sr5010_searchByCmd(char* ptrCmd)
   int i = 0;
   int c = 0;
   int match = 0;
+  char len = 0;
+
+  //take length of each available command string.
+  //compare over length of that string.
+  
   while(i<SR5010_NUMCHARS && !match)
   {
-    c = memcmp(ptrCmd, sr5010Map[i].statusStr, 3);
+    if(sr5010Map[i].numArgs > 0)
+    {
+      len = strlen(sr5010Map[i].statusStr);
+      c = memcmp(ptrCmd, sr5010Map[i].statusStr, len);
+    }
     
     if(c == 0) //we found a match.
     {
@@ -253,23 +317,26 @@ void UIUpdate(char bitField)
 {
   char field, j, argIndex;
   bool refreshNeeded = false;
+  #define NUM_FIELDS 5
+  char cmdIndices[NUM_FIELDS] = {0, 2, 3, 4, 5};
   
-  for(field = 0; field < 5; field++)
+  for(field = 0; field < NUM_FIELDS; field++)
   {
     if(bitField & (1 << field))
     {
       refreshNeeded = true;
 
       argIndex = 0;
-      if(field < 2) //Skip volume and power fields, volume is at index 0 and volume is stored as an integer.
+      if(cmdIndices[field] != 2) //Skip volume field, volume is stored as an integer.
       {
         //find argument string major index
-        for(j = 0; j < field; j++)
+        for(j = 0; j < cmdIndices[field]; j++)
         {
           argIndex += sr5010Map[j].numArgs;
         }
-        argIndex += sr5010MapLocalState[field]; //add argument string minor index
+        argIndex += sr5010MapLocalState[cmdIndices[field]]; //add argument string minor index
       }
+      Serial.printf("argIndex=%d\n", argIndex);
 
       M5.Lcd.fillRect(0, (10*field), M5.Lcd.width()-1, 10, BLACK);
       M5.Lcd.setCursor(0, (10*field));
@@ -281,7 +348,7 @@ void UIUpdate(char bitField)
         break;
 
         case 1: //Volume (stored as integer, not index)
-          M5.Lcd.printf("Volume: %d", sr5010MapLocalState[field]);
+          M5.Lcd.printf("Volume: %d", sr5010MapLocalState[cmdIndices[field]]);
         break;
 
         case 2: //Mute
@@ -290,15 +357,17 @@ void UIUpdate(char bitField)
 
         case 3: //Audio source
           M5.Lcd.printf("Audio: %s", sr5010MapArgsOut[argIndex]);
-        break;
+        break; 
 
         case 4: //Video source
           M5.Lcd.printf("Video: %s", sr5010MapArgsOut[argIndex]);
         break;
 
+        /*
         case 7: //Zone 2
           M5.Lcd.printf("Zone2: %s", sr5010MapArgsOut[argIndex]);
         break;
+        */
 
         default:
         break;
@@ -308,7 +377,6 @@ void UIUpdate(char bitField)
 
   if(refreshNeeded) 
   {
-    //M5.update();
     updateLcd = true;
   }
 }
@@ -333,7 +401,8 @@ void setup() {
   }
 
   // initialize the M5Stack object
-  M5.begin();
+  //NOTE: Something in the M5Stack API makes BLE init unreliable.
+  M5.begin(true, false, false, true);
   /*
     Power chip connected to gpio21, gpio22, I2C device
     Set battery charging voltage and current
@@ -342,6 +411,9 @@ void setup() {
   M5.Power.begin();
   M5.Lcd.setBrightness(200);
   M5.Lcd.setTextWrap(true, true);
+
+  //turn off M5Stack speaker clicking noise.
+  dacWrite(25,0);
 
   M5.Power.setWakeupButton(BUTTON_A_PIN);
   if(M5.Power.canControl())
@@ -369,7 +441,9 @@ void setup() {
   Serial.println("Starting BLE.");
   M5.Lcd.println("Starting BLE.");
   M5.update();
-  BLEDevice::init("");
+  BLEDevice::init("KnobSwitch-Ctrl");
+
+  updateAll = true; //pull all remote characteristics at connection time.
 
   /*
   // Retrieve a Scanner and set the callback we want to use to be informed when we
@@ -409,7 +483,7 @@ void loop() {
     if(sleepTimerExpired)
     {
       //wake up LCD, reconnect to BLE server and process UI input.
-      M5.Lcd.wakeup();
+      //M5.Lcd.wakeup();
       M5.Lcd.setBrightness(200);
       M5.Lcd.fillRect(0, M5.Lcd.height()-21, M5.Lcd.width()-1, 10, BLACK);
       M5.Lcd.setCursor(0, M5.Lcd.height()-21);
@@ -424,8 +498,8 @@ void loop() {
   UIStatePrevious = i;
 
   if(connected)
-  {    
-    //TODO: poke remote characteristics per the control being operated.
+  {
+    //poke remote characteristics per the control being operated.
     char c;
     char txBuf[16];
     std::string val;
@@ -435,23 +509,22 @@ void loop() {
     i = digitalRead(39);
     if(!i) //button A
     {
-      sr5010MapLocalState[0] ^= 1; //toggle power state.
-      c = '0' + sr5010MapLocalState[0];
+      sr5010MapLocalState[1] ^= 1; //toggle power state.
       
-      //Get remote characteristic.
+      //Poke ZM.
       if(pRemoteService != nullptr)
       {
-        pRemoteCharacteristic = pRemoteService->getCharacteristic(sr5010Map[0].uuid);
+        pRemoteCharacteristic = pRemoteService->getCharacteristic(sr5010Map[1].uuid);
         if (pRemoteCharacteristic == nullptr) {
           Serial.println("Can't find characteristic.");
         }
         else
         {
-          Serial.printf("-> %s: ", sr5010Map[0].uuid);
+          Serial.printf("-> %s: ", sr5010Map[1].uuid);
       
           //Write value to remote characteristic.
           if(pRemoteCharacteristic->canWrite()) {
-            sprintf(txBuf, "%c\r", c);
+            sprintf(txBuf, "%d", sr5010MapLocalState[1]);
             Serial.println(txBuf);
             pRemoteCharacteristic->writeValue(txBuf);
           }
@@ -475,20 +548,20 @@ void loop() {
         c = '1'; //volume down.
       }
 
-      //Get remote characteristic.
+      //Poke MV up/down.
       if(pRemoteService != nullptr)
       {
-        pRemoteCharacteristic = pRemoteService->getCharacteristic(sr5010Map[1].uuid);
+        Serial.printf("-> %s: ", sr5010Map[9].uuid);
+        
+        pRemoteCharacteristic = pRemoteService->getCharacteristic(sr5010Map[9].uuid);
         if (pRemoteCharacteristic == nullptr) {
           Serial.println("Can't find characteristic.");
         }
         else
         {
-          Serial.printf("-> %s: ", sr5010Map[1].uuid);
-      
           //Write value to remote characteristic.
           if(pRemoteCharacteristic->canWrite()) {
-            sprintf(txBuf, "%c\r", c);
+            sprintf(txBuf, "%c", c);
             Serial.println(txBuf);
             pRemoteCharacteristic->writeValue(txBuf);
           }
@@ -498,23 +571,22 @@ void loop() {
 
     if(rotary.push()) //rotary encoder button.
     {
-      sr5010MapLocalState[2] ^= 1; //toggle mute state.
-      c = '0' + sr5010MapLocalState[2];
+      sr5010MapLocalState[3] ^= 1; //toggle mute state.
       
-      //Get remote characteristic.
+      //Poke MU.
       if(pRemoteService != nullptr)
       {
-        pRemoteCharacteristic = pRemoteService->getCharacteristic(sr5010Map[2].uuid);
+        Serial.printf("-> %s: ", sr5010Map[3].uuid);
+        
+        pRemoteCharacteristic = pRemoteService->getCharacteristic(sr5010Map[3].uuid);
         if (pRemoteCharacteristic == nullptr) {
           Serial.println("Can't find characteristic.");
         }
         else
         {
-          Serial.printf("-> %s: ", sr5010Map[2].uuid);
-      
           //Write value to remote characteristic.
           if(pRemoteCharacteristic->canWrite()) {
-            sprintf(txBuf, "%c\r", c);
+            sprintf(txBuf, "%d", sr5010MapLocalState[3]);
             Serial.println(txBuf);
             pRemoteCharacteristic->writeValue(txBuf);
           }
@@ -528,25 +600,25 @@ void loop() {
       time_now = millis();
       sleepTimerExpired = false;
       
-      sr5010MapLocalState[3]++; //increment input state.
-      if(sr5010MapLocalState[3] >= sr5010Map[3].numArgs) sr5010MapLocalState[3] = 0;
+      sr5010MapLocalState[4]++; //increment input state.
+      if(sr5010MapLocalState[4] >= sr5010Map[4].numArgs) sr5010MapLocalState[4] = 0;
       
-      c = '0' + sr5010MapLocalState[3];
+      c = '0' + sr5010MapLocalState[4];
       
-      //Get remote characteristic.
+      //Poke SI
       if(pRemoteService != nullptr)
       {
-        pRemoteCharacteristic = pRemoteService->getCharacteristic(sr5010Map[3].uuid);
+        Serial.printf("-> %s: ", sr5010Map[4].uuid);
+        
+        pRemoteCharacteristic = pRemoteService->getCharacteristic(sr5010Map[4].uuid);
         if (pRemoteCharacteristic == nullptr) {
           Serial.println("Can't find characteristic.");
         }
         else
         {
-          Serial.printf("-> %s: ", sr5010Map[0].uuid);
-      
           //Write value to remote characteristic.
           if(pRemoteCharacteristic->canWrite()) {
-            sprintf(txBuf, "%c\r", c);
+            sprintf(txBuf, "%d", sr5010MapLocalState[4]);
             Serial.println(txBuf);
             pRemoteCharacteristic->writeValue(txBuf);
           }
@@ -554,6 +626,40 @@ void loop() {
       }
 
       while(!digitalRead(38)); 
+    }
+
+    i = digitalRead(37);
+    if(!i) //button C
+    {
+      time_now = millis();
+      sleepTimerExpired = false;
+      
+      sr5010MapLocalState[5]++; //increment video input state.
+      if(sr5010MapLocalState[5] >= sr5010Map[5].numArgs) sr5010MapLocalState[5] = 0;
+      
+      c = '0' + sr5010MapLocalState[5];
+      
+      //Poke SV
+      if(pRemoteService != nullptr)
+      {
+        Serial.printf("-> %s: ", sr5010Map[5].uuid);
+        
+        pRemoteCharacteristic = pRemoteService->getCharacteristic(sr5010Map[5].uuid);
+        if (pRemoteCharacteristic == nullptr) {
+          Serial.println("Can't find characteristic.");
+        }
+        else
+        {
+          //Write value to remote characteristic.
+          if(pRemoteCharacteristic->canWrite()) {
+            sprintf(txBuf, "%d", sr5010MapLocalState[5]);
+            Serial.println(txBuf);
+            pRemoteCharacteristic->writeValue(txBuf);
+          }
+        }
+      }
+
+      while(!digitalRead(37)); 
     }
 
     if(0) //selector switch
@@ -594,7 +700,7 @@ void loop() {
     sleepTimerExpired = true;
     Serial.println("Sleeping now.");
     M5.Lcd.setBrightness(0);
-    M5.Lcd.sleep();
+    //M5.Lcd.sleep();
   }
 
   if(updateBitfield)
